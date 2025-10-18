2a08c8e6010296ab6e1d53f7779626aebd97be51
dahalutsab <dlutsab2120@gmail.com>
2025-10-18 19:36:28 +0545

    kafka-consumer

---
+++ analytics_service/src/main/java/com/patient_management/analytics_service/kafka/KafkaConsumer.java
@@ -0,0 +1,30 @@
+package com.patient_management.analytics_service.kafka;
+
+import org.apache.kafka.shaded.com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+
+            log.info("Received Patient Event: [PatientId: {}, PatientName: {}, PatientEmail: {}]",
+                    patientEvent.getPatientId(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail()
+            );
+        } catch (Exception e) {
+            log.error("Failed to parse PatientEvent", e);
+        }
+    }
+}
